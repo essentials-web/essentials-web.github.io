6e21b7145d20fd8e75f10c8b6d8e8d78c51f59bc
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-22 12:25:42 -0500

    Update search.js

--- scripts/simple/search.js
+++ scripts/simple/search.js
@@ -1,6 +1,12 @@
 function search(){
   var bar = document.getElementsByClassName("search").value;
+  var result1 = document.getElementById("result1");
+  var result2 = document.getElementById("result2");
+  var result3 = document.getElementById("result3");
+  var result4 = document.getElementById("result4");
+  var result5 = document.getElementById("result5");
   if(bar == "Unit Converter Websites"){
-    
+    result1.href = "https://www.unitconverters.net/";
+    result1.innerHTML = "Unit Converter";
   }
 }
